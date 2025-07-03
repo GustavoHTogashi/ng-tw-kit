5940620a6a6813e2d669a1e71e703074a280d5ac
Gustavo Togashi <ghtogashi@gmail.com>
2025-07-03 01:29:58 -0300

    test: button

--- libs/directives/button/src/button.spec.ts
+++ libs/directives/button/src/button.spec.ts
@@ -1,21 +1,39 @@
-import { ButtonDirective } from './button';
-import { render, RenderResult } from '@testing-library/angular';
+import { render } from '@testing-library/angular';
+import { NgtwButton } from './button';
 
 describe('directive:button', () => {
-  let container: RenderResult<unknown, unknown>;
-  let buttonElement: HTMLElement;
-
-  beforeEach(async () => {
-    container = await render(`<button ngtw></button>`, {
-      imports: [ButtonDirective],
-      componentProperties: {},
+  it('should render a button', async () => {
+    const container = await render(`<button ngtwButton></button>`, {
+      imports: [NgtwButton],
     });
+    const buttonElement = container.getByRole('button');
 
-    buttonElement = container.getByRole('button');
-  });
-
-  it('should render a button', () => {
     expect(buttonElement).toBeTruthy();
     expect(buttonElement.tagName).toBe('BUTTON');
+    expect(buttonElement.classList.contains('cursor-pointer')).toBe(true);
+    expect(buttonElement.classList.contains('rounded')).toBe(true);
+    expect(buttonElement.classList.contains('bg-zinc-800')).toBe(true);
+    expect(buttonElement.classList.contains('px-2')).toBe(true);
+    expect(buttonElement.classList.contains('py-2')).toBe(true);
+    expect(buttonElement.classList.contains('text-current')).toBe(true);
+    expect(
+      buttonElement.classList.contains(
+        'transition-[background-color,_translate,_opacity]',
+      ),
+    ).toBe(true);
+    expect(buttonElement.classList.contains('outline-none')).toBe(true);
+    expect(buttonElement.classList.contains('select-none')).toBe(true);
+    expect(buttonElement.classList.contains('hover:bg-zinc-800/50')).toBe(true);
+    expect(buttonElement.classList.contains('focus-visible:ring-2')).toBe(true);
+    expect(
+      buttonElement.classList.contains('focus-visible:ring-purple-500'),
+    ).toBe(true);
+    expect(buttonElement.classList.contains('active:translate-y-0.5')).toBe(
+      true,
+    );
+    expect(
+      buttonElement.classList.contains('disabled:pointer-events-none'),
+    ).toBe(true);
+    expect(buttonElement.classList.contains('disabled:opacity-25')).toBe(true);
   });
 });
