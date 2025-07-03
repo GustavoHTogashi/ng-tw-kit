af36c733a0f7d29078c6637c884a81dca801396e
Gustavo Togashi <ghtogashi@gmail.com>
2025-07-03 01:37:44 -0300

    refactor: button

--- libs/directives/button/src/button.ts
+++ libs/directives/button/src/button.ts
@@ -3,12 +3,12 @@ import { Directive, signal } from '@angular/core';
 @Directive({
   exportAs: 'ngtwButton',
   host: {
-    '[class]': 'hostClasses()',
+    '[class]': 'hostClass()',
   },
   selector: 'button[ngtwButton]',
 })
 export class NgtwButton {
-  protected hostClasses = signal(
+  protected hostClass = signal(
     'cursor-pointer rounded bg-zinc-800 px-2 py-2 text-current transition-[background-color,_translate,_opacity] outline-none select-none hover:bg-zinc-800/50 focus-visible:ring-2 focus-visible:ring-purple-500 active:translate-y-0.5 disabled:pointer-events-none disabled:opacity-25',
   );
 }
